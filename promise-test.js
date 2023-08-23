31fccbe30e53572d2e4ae7995df7c43547e1ed10
mizuki-code <dogwooood@gmail.com>
2023-08-23 23:18:07 +0900

    解説追加

--- promise-test.js
+++ promise-test.js
@@ -1,10 +1,13 @@
 'use strict';
 
+// 最初のPromiseでnew Date()の結果を、resolve関数で指定している
 new Promise((resolve) => {
     const nowDate = new Date();
     resolve(nowDate);    
 }).then((v1) => {
     // v1 は 現在の時刻情報
+    // 最初のPromiseのnew Date()の結果がv1に代入される
+    // 引数v1から月と日を取得して、monthAndDateオブジェクトを作る
     new Promise((resolve) => {
         const monthAndDate = {
             month: v1.getMonth(),
@@ -13,11 +16,13 @@ new Promise((resolve) => {
         resolve(monthAndDate);
     }).then((v2) => {
         // v2 は 日付の情報
+        // 2番目のProject のmonthAndDateオブジェクトをv2に渡している
         new Promise((resolve) => {
             const text = `今日は${v2.month+1}月${v2.date}日です。`;
             resolve(text);
         }).then((v3) => {
             // v3 は　日付を示す文章
+
             console.log(v3); // 今日の日付に関する文章が出力される
         });
     });
