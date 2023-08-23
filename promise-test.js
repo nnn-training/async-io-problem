8f50387430e3890c53191b980f34c609dc3670ab
mizuki-code <dogwooood@gmail.com>
2023-08-23 22:44:47 +0900

    Promiseを使ってみよう

--- promise-test.js
+++ promise-test.js
@@ -1,8 +1,22 @@
 'use strict';
 
-const waitPromise = new Promise((resolve, reject) => {
-    setTimeout(() => resolve(), 1000);
+const https = require('node:https');
+const fs = require('node:fs');
+let data = '';
+
+const promise = new Promise((resolve,reject) =>{
+    https.get('https://www.nicovideo.jp/ranking/genre/all?term=hour&rss=2.0&lang=ja-jp', (response) =>
+    {
+        response
+            .on('data', (chunk) => {
+                data += chunk;
+            })
+            .on('end', () => {
+                resolve();
+            });
+    })
 });
 
-waitPromise.then(() => console.log('hoge'));
-console.log('fuga');
+promise.then(() => {
+    fs.writeFile('test.txt', data, 'utf8', () => {})
+});
