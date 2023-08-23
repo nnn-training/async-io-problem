53247a5aad1f3a220f5b557366c2e108c33afb06
mizuki-code <dogwooood@gmail.com>
2023-08-23 23:31:36 +0900

    Promiseチェーンを使ってみよう

--- promise-test.js
+++ promise-test.js
@@ -4,26 +4,29 @@
 new Promise((resolve) => {
     const nowDate = new Date();
     resolve(nowDate);    
-}).then((v1) => {
-    // v1 は 現在の時刻情報
-    // 最初のPromiseのnew Date()の結果がv1に代入される
-    // 引数v1から月と日を取得して、monthAndDateオブジェクトを作る
-    new Promise((resolve) => {
+})
+    .then((v1) => {
+        // v1 は 現在の時刻情報
+        // 最初のPromiseのnew Date()の結果がv1に代入される
+        // 引数v1から月と日を取得して、monthAndDateオブジェクトを作る
         const monthAndDate = {
             month: v1.getMonth(),
             date: v1.getDate()
         }
-        resolve(monthAndDate);
-    }).then((v2) => {
+        return new Promise((resolve) => {
+            resolve(monthAndDate);
+        });
+    })
+    // return を使うことで、then 関数の後ろに then 関数をつなげて書けるようになった
+    .then((v2) => {
         // v2 は 日付の情報
         // 2番目のProject のmonthAndDateオブジェクトをv2に渡している
-        new Promise((resolve) => {
-            const text = `今日は${v2.month+1}月${v2.date}日です。`;
+        const text = `今日は${v2.month+1}月${v2.date}日です。`;
+        return new Promise((resolve) => {
             resolve(text);
-        }).then((v3) => {
-            // v3 は　日付を示す文章
-
-            console.log(v3); // 今日の日付に関する文章が出力される
         });
+    })
+    .then((v3) => {
+        // v3 は　日付を示す文章
+        console.log(v3); // 今日の日付に関する文章が出力される
     });
-});
