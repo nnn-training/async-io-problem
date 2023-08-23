30a23a6039edce33426a6b491765111bd20f7e8a
mizuki-code <dogwooood@gmail.com>
2023-08-24 00:17:57 +0900

    Promiseチェーンを利用した書き直し

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
 'use strict';
-
-// const https = require('node:https');
-// const fs = require('node:fs');
-// let data = '';
-
-// https.get('https://www.nicovideo.jp/ranking/genre/all?term=hour&rss=2.0&lang=ja-jp', (response) => {
-//   response
-//     .on('data', (chunk) => {
-//       data += chunk;
-//     })
-//     .on('end', () => {
-//     });
-// });
-
-// fs.writeFile('test.txt', data, 'utf8', () => {});
-
-'use strict';
 const fs = require('fs');
 const fileName = './test.txt';
+
+function appendFilePromise(fileName, str) {
+  return new Promise((resolve) => {
+    fs.appendFile(fileName, str, 'utf8', () => resolve());
+  });
+}
+
 for (let count = 0; count < 500; count++) {
-  fs.appendFileSync(fileName, 'あ', 'utf8');
-  fs.appendFileSync(fileName, 'い', 'utf8');
-  fs.appendFileSync(fileName, 'う', 'utf8');
-  fs.appendFileSync(fileName, 'え', 'utf8');
-  fs.appendFileSync(fileName, 'お', 'utf8');
-  fs.appendFileSync(fileName, '\n', 'utf8');
+  appendFilePromise(fileName, 'あ')
+      .then(() => {
+        return appendFilePromise(fileName, 'い');
+      })
+      .then(() => {
+        return appendFilePromise(fileName, 'う');
+      })
+      .then(() => {
+        return appendFilePromise(fileName, 'え');
+      })
+      .then(() => {
+        return appendFilePromise(fileName, 'お');
+      })
+      .then(() => {
+        return appendFilePromise(fileName, '\n');
+      });
 }
