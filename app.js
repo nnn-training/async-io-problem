032c577bb535106a3394882edb19eef46dfa0586
aurora1530 <35615953+aurora1530@users.noreply.github.com>
2026-04-25 18:01:29 +0900

    feat: ニコニコ動画のランキング取得をRSSからAPI利用に変更

--- app.js
+++ app.js
@@ -4,7 +4,19 @@ const https = require('node:https');
 const fs = require('node:fs');
 let data = '';
 
-https.get('https://www.nicovideo.jp/ranking/genre/all?term=hour&rss=2.0&lang=ja-jp',
+const apiEndpoint ='https://snapshot.search.nicovideo.jp/api/v2/snapshot/video/contents/search';
+const search_keyword_param = 'q=ゲーム';
+const search_targets_param = 'targets=tags';
+const sort_param = '_sort=viewCounter';
+const fields_param =
+  'fields=contentId,title,description,tags,viewCounter,mylistCounter,likeCounter,lengthSeconds,commentCounter';
+const filters_param =
+  'filters[startTime][gte]=2025-05-13T00:00:00Z&filters[startTime][lte]=2025-05-13T23:59:59Z';
+const limit_param = '_limit=5';
+
+const apiRequestUrl = `${apiEndpoint}?${search_keyword_param}&${search_targets_param}&${sort_param}&${fields_param}&${filters_param}&${limit_param}`;
+
+https.get(apiRequestUrl,
   {
     headers: {
       'User-Agent': 'node',
@@ -16,7 +28,7 @@ https.get('https://www.nicovideo.jp/ranking/genre/all?term=hour&rss=2.0&lang=ja-
         data += chunk;
       })
       .on('end', () => {
-        
+
       });
   }
 );
